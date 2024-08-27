ce225f9595b8ba58371aae1424c8ab6973e9260f
Dana Pinzaru <dana.pinzaru@totalsoft.ro>
2024-08-27 16:06:59 +0300

    Conference Request

---
+++ Requests/ConferenceRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConferenceAPI.Models;
+
+public partial class ConferenceRequest
+{
+    public int ConferenceTypeId { get; set; }
+
+    public int LocationId { get; set; }
+
+    public string OrganizerEmail { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public string Name { get; set; } = null!;
+}
